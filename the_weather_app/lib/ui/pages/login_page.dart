40cd09c23afac7d35a1d7dc18c3253983907daad
Rosl9kov <rosliakov.tolik@gmail.com>
2023-05-05 22:23:41 +0300

    configured the input validation field

--- the_weather_app/lib/ui/pages/login_page.dart
+++ the_weather_app/lib/ui/pages/login_page.dart
@@ -1,25 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:the_weather_app/domain/bloc/weather_bloc.dart';
+import 'package:the_weather_app/domain/bloc/weather_event.dart';
 import 'package:the_weather_app/ui/theme/app_button.dart';
-import '../widgets/text_form_widget.dart';
+import '../theme/app_colors.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key, required this.error});
+  final String error;
+  TextEditingController textController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? _validateName(String? value) {
+    // Проверяем, что значение не пустое
+    if (value == null || value.isEmpty) {
+      return 'Please enter the name of the city';
+    }
+
+    // Проверяем, что значение содержит только буквы
+    final RegExp regex = RegExp(r'^[a-zA-Z]+$');
+    if (!regex.hasMatch(value)) {
+      return 'Please enter a valid name';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final WeatherBloc weatherBloc = BlocProvider.of<WeatherBloc>(context);
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            TextFormWidget(),
-            AppButton(
-                text: 'Вход',
-                func: () {
-                  context.go('/detail');
-                })
-          ]),
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                'Weather App',
+                style: TextStyle(
+                    color: AppColors.defaultColor3,
+                    fontWeight: FontWeight.w100,
+                    fontSize: 60),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                controller: textController,
+                validator: _validateName,
+                maxLength: 30,
+                keyboardType: TextInputType.text,
+                inputFormatters: [
+                  FilteringTextInputFormatter.singleLineFormatter
+                ],
+                decoration: const InputDecoration(
+                    labelText: 'Search',
+                    labelStyle: TextStyle(color: AppColors.defaultColor2),
+                    hintText: 'Moscow',
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: AppColors.defaultColor2)),
+                    focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: AppColors.defaultColor4)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide:
+                            BorderSide(color: AppColors.defaultColor3))),
+              ),
+              AppButton(
+                  text: 'GO',
+                  func: () {
+                    if (_formKey.currentState!.validate()) {
+                      weatherBloc
+                          .add(WeatherLoadEvent(cityName: textController.text));
+                      context.go('/detail');
+                    }
+                  }),
+              Text(
+                error,
+                style: const TextStyle(color: AppColors.defaultColor4),
+              )
+            ]),
+          ),
         ),
       ),
     );
